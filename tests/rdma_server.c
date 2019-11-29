e7b7a53e9e16a4f9d6e82ccd7234fc57ec912a19
Rajath Shashidhara <rajath.shashidhara@gmail.com>
2019-11-29 17:34:51 -0600

    Modify RDMA server test appliction to perform rdma_write()
    Signed-off-by: Rajath Shashidhara <rajath.shashidhara@gmail.com>

--- tests/rdma_server.c
+++ tests/rdma_server.c
@@ -8,7 +8,7 @@
 
 int main()
 {
-    const char ip[] = "10.0.0.1";
+    const char ip[] = "10.0.0.101";
     rdma_init();
     struct sockaddr_in localaddr, remoteaddr;
     localaddr.sin_family = AF_INET;
@@ -23,5 +23,21 @@ int main()
 
     if (fd < 0)
         fprintf(stderr, "Connection failed\n");
+
+    const char name[] = "RAJATH";
+    int i = 0;
+    char* new_mr_base = mr_base;
+    while (1)
+    {
+      int len = snprintf(new_mr_base, 100, "%s%u", name, i);
+      int ret = rdma_write(fd, len, new_mr_base - (char*) mr_base, 0);
+      new_mr_base += len;
+      fprintf(stderr, "WRITE ret=%d\n", ret);
+
+      if (ret < 0)
+        break;
+      i++;
+    }
+
     return 0;
 }
