2bae4f1fbb29564bac9675038cbbff2e5f333402
Shailesh Mani Pandey <manipandey.shailesh@gmail.com>
2019-12-08 13:23:57 -0600

    lib/tas/init: Fix errors on complete wq consumption

--- lib/tas/init.c
+++ lib/tas/init.c
@@ -225,17 +225,19 @@ int rdma_fastpath_poll(struct flextcp_context *ctx,
             break;
         } else if (arx->type == FLEXTCP_PL_ARX_RDMAUPDATE) {
             rx_conn = OPAQUE_PTR(arx->msg.rdmaupdate.opaque);
-            if (arx->msg.rdmaupdate.wq_tail >= rx_conn->wq_tail){
-                rx_conn->wq_len -= (arx->msg.rdmaupdate.wq_tail - rx_conn->wq_tail);
-            }else{
-                rx_conn->wq_len -= (arx->msg.rdmaupdate.wq_tail + rx_conn->wq_size - rx_conn->wq_tail);
-            }
-            rx_conn->wq_tail = arx->msg.rdmaupdate.wq_tail;
             if (arx->msg.rdmaupdate.cq_head > rx_conn->cq_tail){
                 rx_conn->cq_len = arx->msg.rdmaupdate.cq_head - rx_conn->cq_tail;
             }else{
                 rx_conn->cq_len = arx->msg.rdmaupdate.cq_head + rx_conn->wq_size - rx_conn->cq_tail;
             }
+            if (arx->msg.rdmaupdate.wq_tail > rx_conn->wq_tail){
+                rx_conn->wq_len -= (arx->msg.rdmaupdate.wq_tail - rx_conn->wq_tail);
+            }else if (arx->msg.rdmaupdate.wq_tail < rx_conn->wq_tail){
+                rx_conn->wq_len -= (arx->msg.rdmaupdate.wq_tail + rx_conn->wq_size - rx_conn->wq_tail);
+            }else if (rx_conn->wq_len == rx_conn->wq_size){
+                rx_conn->wq_len = 0;
+            }
+            rx_conn->wq_tail = arx->msg.rdmaupdate.wq_tail;
             i = conn->cq_len;
         } else {
             fprintf(stderr, "flextcp_context_poll: kout type=%u head=%x\n", arx->type, head);
